c74203f3d4b4f6ba61868892d908b3067ee39365
yemregumus <ygumus@myseneca.ca>
2024-04-13 22:40:37 -0400

    connecting argocd to repo

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const nodemailer = require("nodemailer");
 const cors = require("cors");
 
 const app = express();
-const port = process.env.PORT || 3001;
+const port = 3001;
 
 // Middleware to enable CORS
 app.use(cors());
